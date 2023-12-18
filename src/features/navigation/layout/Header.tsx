d0b9d7f834bb0f1d6442569d4541094b54ae915b
giacomotolari <tolarigiacomo@gmail.com>
2023-12-18 12:17:17 +0100

    feature: NavLinks List

--- src/features/navigation/layout/Header.tsx
+++ src/features/navigation/layout/Header.tsx
@@ -1,6 +1,4 @@
-import { path } from "features/navigation/routing/path";
-import {} from "react";
-import { NavLink } from "react-router-dom";
+import LinksList from "./LinksList";
 
 export default function Header() {
   return (
@@ -11,36 +9,7 @@ export default function Header() {
             className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
             id="mobile-menu-2"
           >
-            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-              <li className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
-                <NavLink
-                  to={path.home}
-                  className={({ isActive, isPending }) =>
-                    isPending
-                      ? "text-slate-400"
-                      : isActive
-                      ? "text-slate-900"
-                      : ""
-                  }
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
-                <NavLink
-                  to={path.register}
-                  className={({ isActive, isPending }) =>
-                    isPending
-                      ? "text-slate-400"
-                      : isActive
-                      ? "text-slate-900"
-                      : ""
-                  }
-                >
-                  Register
-                </NavLink>
-              </li>
-            </ul>
+           <LinksList />
           </div>
         </div>
       </nav>
