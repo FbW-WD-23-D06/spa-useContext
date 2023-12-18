4518d0299c778a60b7d41538df675c1ad9f9fd44
giacomotolari <tolarigiacomo@gmail.com>
2023-12-18 11:42:30 +0100

    feature: routing basic system

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import Home from 'pages/Home'
-import Login from 'pages/Login'
-import Profile from 'pages/Profile'
-import Register from 'pages/Register'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Home from "pages/Home";
+import Login from "pages/Login";
+import Profile from "pages/Profile";
+import Register from "pages/Register";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "features/navigation/layout/Layout.tsx";
+import { path } from "features/navigation/routing/path.ts";
+import NotFound from "pages/NotFound.tsx";
 
+const router = createBrowserRouter([
+  {
+    path: path.home,
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: path.register,
+        element: <Register />,
+      },
+      {
+        path: path.login,
+        element: <Login />,
+      },
+      {
+        path: path.profile,
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} fallbackElement={<p>loading..</p>} />
+  </React.StrictMode>
+);
