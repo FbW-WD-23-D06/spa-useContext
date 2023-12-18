25b711a4593575e1969f31c96bba9d9070316705
giacomotolari <tolarigiacomo@gmail.com>
2023-12-18 23:59:16 +0100

    refactor: move router into an external file

--- src/main.tsx
+++ src/main.tsx
@@ -1,44 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import Home from "pages/Home";
-import Login from "pages/Login";
-import Profile from "pages/Profile";
-import Register from "pages/Register";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Layout from "features/navigation/layout/Layout.tsx";
-import { path } from "features/navigation/routing/path.ts";
-import NotFound from "pages/NotFound.tsx";
+import { RouterProvider } from "react-router-dom";
 import { ThemeContextProvider } from "./contexts/ThemeContext";
-
-const router = createBrowserRouter([
-  {
-    path: path.home,
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: path.register,
-        element: <Register />,
-      },
-      {
-        path: path.login,
-        element: <Login />,
-      },
-      {
-        path: path.profile,
-        element: <Profile />,
-      },
-    ],
-  },
-  {
-    path: "*",
-    element: <NotFound />,
-  },
-]);
+import { router } from "./features/navigation/routing/router";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
