acbea0f3a5b1f95cbcb63d657593e61d00270083
giacomotolari <tolarigiacomo@gmail.com>
2023-12-18 16:05:30 +0100

    style: header and darkModeToggle styles

--- src/features/navigation/layout/header/DarkModeToggle.tsx
+++ src/features/navigation/layout/header/DarkModeToggle.tsx
@@ -1,14 +1,13 @@
 import { useThemeContext } from "@/contexts/ThemeContext";
-import {} from "react";
 import { MdWbSunny, MdModeNight } from "react-icons/md";
 
 export default function DarkModeToggle() {
   const { darkMode, setDarkMode } = useThemeContext()!;
 
   const icon = darkMode ? (
-    <MdWbSunny className="text-2xl" />
+    <MdWbSunny className="text-2xl" color="yellow" />
   ) : (
-    <MdModeNight className="text-2xl" />
+    <MdModeNight className="text-2xl" color="white" />
   );
 
   const handleDarkModeToggle = () => setDarkMode(!darkMode);
--- src/features/navigation/layout/header/Header.tsx
+++ src/features/navigation/layout/header/Header.tsx
@@ -4,15 +4,8 @@ import LinksList from "./LinksList";
 export default function Header() {
   return (
     <header>
-      <nav className="bg-orange-200 border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <div
-            className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-            id="mobile-menu-2"
-          >
-            <LinksList />
-          </div>
-        </div>
+      <nav className="bg-orange-200 border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 flex justify-between">
+        <LinksList />
         <DarkModeToggle />
       </nav>
     </header>
