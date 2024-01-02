ee630ff56b8791f01000ab5aa10eb4c763920fd6
giacomotolari <tolarigiacomo@gmail.com>
2024-01-02 10:24:21 +0100

    feature: add UserContext provider in main,jsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import { RouterProvider } from "react-router-dom";
 import { ThemeContextProvider } from "./contexts/ThemeContext";
 import { router } from "./features/navigation/routing/router";
 import "./index.css";
+import { UserContextProvider } from "./contexts/UserContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeContextProvider>
-      {/* Wrap the entire app with the ThemeContextProvider to be able to use the state from the provider throughout the app */}
-      <RouterProvider router={router} fallbackElement={<p>loading..</p>} />
+      <UserContextProvider>
+        {/* Wrap the entire app with the ThemeContextProvider to be able to use the state from the provider throughout the app */}
+        <RouterProvider router={router} fallbackElement={<p>loading..</p>} />
+      </UserContextProvider>
     </ThemeContextProvider>
   </React.StrictMode>
 );
